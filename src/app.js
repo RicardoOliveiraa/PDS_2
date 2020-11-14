3a0421c0efcff19488455c1d6b54a41553bfb4bb
LuizEduardoHorbeAlves <luizeduardohorbe@furg.br>
2020-11-13 22:48:25 -0300

    Routes/update app.js/updatesgnup
    Nesta etapa foi realizado uma edição no signup, foi adicionado ao signup confirmação de senha e outras modificações pequenas.
    Após, foi criada a pasta helpers que possui o arquivo routes.js, no routes.js foram denifinidos os IsUserRedirect, ProtectedRoute, que fazem a rota para o usuário logado,  após este step, foi modificado o app.js, onde foram importados IsUserRedirect, ProtectedRoute para o app.js, após importar as rotas corrigidas, foram alteradas as rotas para as paginas utilizando o IsUserRedirect. Qualquer dúvida, só chamar ^^

--- src/app.js
+++ src/app.js
@@ -2,23 +2,39 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home, Browse, Signin, Signup } from './pages';
 import * as ROUTES from './constants/routes';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 
 export default function App() {
-  return (<Router>
-    <Route exact path="/browse">
-      <Browse />
-    </Route>
-    <Route exact path="/signin">
-      <Signin />
-    </Route>
-    <Route exact path="/signup">
-      <Signup />
-    </Route>
-    <Route exact path={ROUTES.HOME}>
-      <Home />
-    </Route>
+  const user = null;
+  /*
+  const user = {name: 'luiz'}; = usuario logado => browse
+  const user = null; = não há um usuario logado => se mantem na page por nao estar logado
+ 
+  */
+  return (
+  <Router>
+      <IsUserRedirect 
+      user={user}
+      loggedInPath={ROUTES.BROWSE} 
+      path={ROUTES.SIGN_IN}>
+        <Signin />
+      </IsUserRedirect>
+      <IsUserRedirect 
+      user={user} 
+      loggedInPath={ROUTES.BROWSE} 
+      path={ROUTES.SIGN_UP}>
+        <Signup />
+      </IsUserRedirect>
+      <ProtectedRoute 
+      user={user} 
+      path={ROUTES.BROWSE}>
+        <Browse />
+      </ProtectedRoute>
+      <IsUserRedirect user={user} 
+      loggedInPath={ROUTES.BROWSE} 
+      path={ROUTES.HOME}>
+        <Home />
+      </IsUserRedirect>
   </Router>
   );
 }
-
-
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,5 +1,73 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Form } from '../components';
+import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
+import * as ROUTES from '../constants/routes';
 
-export default function Signup() {
-    return <p>E ai, estamos em SignUp</p>
+export default function SignUp() {
+  const history = useHistory();
+
+  const [firstName, setFirstName] = useState('');
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmation, setConfirmation] = useState('');
+  const [error, setError] = useState('');
+
+  const isInvalid = firstName === '' || emailAddress === '' || password === '' || confirmation === '';
+
+  const handleSignup = (event) => {
+    event.preventDefault();
+  }
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign Up</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignup} method="POST">
+            <Form.Input
+              placeholder="Nome"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
+            <Form.Input
+              placeholder="Email"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              value={password}
+              autoComplete="off"
+              placeholder="Senha"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Input
+              type="confirmation"
+              value={confirmation}
+              autoComplete="off"
+              placeholder="Confirme a senha"
+              onChange={({ target }) => setConfirmation(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit" data-testid="sign-up">
+              Inscrição
+            </Form.Submit>
+          </Form.Base>
+
+          <Form.Text>
+            Já é usuário da DisneyFlix? <Form.Link to="/signin">Faça seu login.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+          Esta página é protegida pelo Google reCAPTCHA para garantir que você não seja um robô.
+          <Form.Link to="">Saiba mais.</Form.Link>
+          </Form.TextSmall>
+          
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
