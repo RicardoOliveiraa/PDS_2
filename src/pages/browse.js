3728d58530b8363f71643146e21f2292ed1976d7
Ricardo Oliveira <ricardoooliveiraa@outlook.com>
2020-12-14 20:58:54 -0300

    tretas que o erick fez comigo no visual
    Adiciona arquivos e mudanças para inciar o header da páginas logado

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 export default function Browse() {
-    return (
-        <>
-            <p>E ai, estamos em Browse</p>
-        </>
-    );
+    var series = {title:"serie quente", slug:"arthurS", genre:"children", id:"272727272727ab", maturity: "0", description:"a descrição mais braba das series"};
+    var films = {title: "filme massa", slug:"Filmão", genre:"children", id:"272727272727ab", maturity: "0", description:"a descrição mais braba dos filmes"};
+    
+
+
+    return <p> ALOU MUNDO </p>;
 }
